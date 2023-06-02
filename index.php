1679b75b4c8fc7a8a3a3ec61658ec80dd42dd9f5
halvaradop_ <hernanvid123@gmail.com>
2023-06-02 00:03:22 -0500

    implementacion dao inventario

--- index.php
+++ index.php
@@ -2,9 +2,13 @@
 
     require_once 'model/usuario.php';
     require_once 'model/proveedor.php';
+    require_once 'model/inventario.php';
     include './persistence/usuarioDAO.php';
     include './persistence/provedorDAO.php';
+    include './persistence/inventarioDAO.php';
 
     $usuarioDao = new UsuarioDAO();
     $proveedorDao = new ProveedorDAO();
+    $inventarioDao = new InventarioDAO();
+    
 ?> 
--- persistence/inventarioDAO.php
+++ persistence/inventarioDAO.php
@@ -1,7 +1,7 @@
 <?php
 
-    require_once '../model/inventario.php';
     require_once 'crud.php';
+    require_once 'conexion.php';
 
     class InventarioDAO implements CRUD {
 
@@ -11,29 +11,65 @@
         public function __construct() {
             $conexion = new Conexion();
             $this -> link = $conexion -> getConexion();
-            $this -> listaInventario = null;
+            $this -> listaInventario = array();
         }
 
-        public function agregar() {
-
+        public function agregar($nuevoInventario) {
+            try {
+                $query = "INSERT INTO Inventario VALUES(null, :nombre, :precio, :cantidad)";
+                $statement = $this -> link -> prepare($query);
+                $statement -> bindValue(':nombre', $nuevoInventario -> getNombre());
+                $statement -> bindValue(':precio', $nuevoInventario -> getPrecio());
+                $statement -> bindValue(':cantidad', $nuevoInventario -> getCantidad());
+                $statement -> execute();
+                $this -> mostrarLista();
+            } catch (PDOException $error) {
+                echo "Error al obtener los datos del inventario: " . $error -> getMessage();
+                return null;
+            }
         }
 
-        public function eliminar() {
-
+        public function eliminar($idInventario) {
+            try {
+                $query = "DELETE FROM Inventario WHERE id_inventario = :id";
+                $statement = $this -> link -> prepare($query);
+                $statement -> bindValue(':id', $idInventario);
+                $statement -> execute();
+                $this -> mostrarLista();
+            }catch (PDOException $error) {
+                echo "Error al obtener los datos del inventario: " . $error -> getMessage();
+                return null;
+            }
         }
 
-        public function actualizar() {
-
+        public function actualizar($idInventario, $nuevoInventario) {
+            try {
+                $query = "UPDATE Inventario SET nombre = :nombre, precio = :precio, cantidad = :cantidad WHERE id_inventario = :id";
+                $statement = $this -> link -> prepare($query);
+                $statement -> bindValue(':id', $idInventario);
+                $statement -> bindValue(':nombre', $nuevoInventario -> getNombre());
+                $statement -> bindValue(':precio', $nuevoInventario -> getPrecio());
+                $statement -> bindValue(':cantidad', $nuevoInventario -> getCantidad());
+                $statement -> execute();
+                $this -> mostrarLista();
+            } catch (PDOException $error) {
+                echo "Error al obtener los datos del inventario: " . $error -> getMessage();
+                return null;
+            }
         }
         
         public function mostrarLista() {
             try {
-                $query = "SELECT * FROM inventario";
-                $stmt = $this->db->query($query);
-                return $stmt->fetchAll(PDO::FETCH_ASSOC);
-            } catch(PDOException $e) {
-                echo "Error al obtener los datos del inventario: " . $e->getMessage();
-                return null;
+                $query = "SELECT * FROM Inventario";
+                $statement = $this -> link -> query($query);
+                $results = $statement -> fetchAll(PDO::FETCH_ASSOC);
+                foreach($results as $value) {
+                    $this -> listaInventario[] = new Inventario($value['id_inventario'], $value['nombre'], $value['precio'], $value['cantidad']);
+                }
+                return $this -> listaInventario;
+            } catch(PDOException $error) {
+                echo "Error al obtener los datos del inventario: " . $error -> getMessage();
+                return array();
             }
         }
     }
