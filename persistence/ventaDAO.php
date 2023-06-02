606859494533056176a81b07c373ff801c0cb409
EstebanHdez1 <91765395+EstebanHdez1@users.noreply.github.com>
2023-06-02 14:46:57 -0500

    a

--- persistence/ventaDAO.php
+++ persistence/ventaDAO.php
@@ -29,17 +29,48 @@
             }
         }
 
-        public function eliminar() {
+        public function eliminar($idVenta) {
+			try {
+				$query = "DELETE FROM Venta WHERE id_venta = :idVenta";
+				$statement = $this->link->prepare($query);
+				$statement->bindValue(':idVenta', $idVenta);
+				$statement->execute();
+				return $statement->rowCount();
+			} catch (PDOException $error) {
+				echo "Error al eliminar la venta: " . $error->getMessage();
+				return 0;
+			}
+		}
 
-        }
 
-        public function actualizar() {
-
-        }
+        public function actualizar($venta) {
+			try {
+				$query = "UPDATE Venta SET fecha_venta = :fechaVenta, total_venta = :totalVenta WHERE id_venta = :idVenta";
+				$statement = $this->link->prepare($query);
+				$statement->bindValue(':fechaVenta', $venta->getFechaVenta());
+				$statement->bindValue(':totalVenta', $venta->getTotalVenta());
+				$statement->bindValue(':idVenta', $venta->getIdVenta());
+				$statement->execute();
+				return $statement->rowCount();
+			} catch (PDOException $error) {
+				echo "Error al actualizar la venta: " . $error->getMessage();
+				return 0;
+			}
+		}
         
         public function mostrarLista() {
-
-        }
+			try {
+				$query = "SELECT * FROM Venta";
+				$statement = $this->link->query($query);
+				$results = $statement->fetchAll(PDO::FETCH_ASSOC);
+				foreach ($results as $value) {
+					$this->listaVentas[] = new Venta($value['fecha_venta'], $value['total_venta'], $value['lista_productos']);
+				}
+				return $listaVentas;
+			} catch (PDOException $error) {
+				echo "Error al obtener los datos de las ventas: " . $error->getMessage();
+				return [];
+			}
+		}
     }
-
 ?>
