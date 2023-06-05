c087388896a22ef968fb07789866e2bfdff6923a
EstebanHdez1 <91765395+EstebanHdez1@users.noreply.github.com>
2023-06-05 03:02:19 -0500

    TelefonoDAO

--- PapeleriaWeb/lista-compra.php
+++ PapeleriaWeb/lista-compra.php
@@ -1,19 +1,21 @@
 <?php
-    require_once '../persistence/usuarioDAO.php';
-    $usuarioDao = new UsuarioDAO();
-    $lista = $usuarioDao -> mostrarLista();
+    require_once 'compraDAO.php';
+
+    $compraDao = new CompraDAO();
+    $lista = $compraDao->mostrarLista();
     $size = count($lista);
-    foreach($lista as $usuario) {
+    
+    foreach($lista as $compra) {
         echo "<tr>";
         echo "<td>";
         echo "<button id='modify-fila' style='border: none; background-color: transparent;' onclick='modificarFormularioEmpleados()'><i class='bx bxs-edit-alt'></i></button>";
         echo "<button id='modify-fila' style='border: none; background-color: transparent;' onclick='eliminarFormularioEmpleados()'><i class='bx bxs-trash'></i></button>";
         echo "</td>";
-        echo "<td>" . $usuario -> getNombres() . "</td>";
-        echo "<td>" . $usuario -> getApellidos() . "</td>";
-		echo "<td>" . $usuario -> getRole() . "</td>";
-        echo "<td>" . $usuario -> getCorreo() . "</td>";
-        echo "<td>" . $usuario -> getFechaNacimiento() . "</td>";
+        echo "<td>" . $compra->getIdCompra() . "</td>";
+        echo "<td>" . $compra->getIdPedido() . "</td>";
+        echo "<td>" . $compra->getFecha() . "</td>";
+        echo "<td>" . $compra->getMontoTotal() . "</td>";
+        echo "<td>" . $compra->getEstado() . "</td>";
         echo "</tr>";
     } 
 ?>
---
+++ persistence/telefonoDAO.php
@@ -0,0 +1,107 @@
+<?php
+
+require_once 'crud.php';
+require_once 'conexion.php';
+require_once 'DAOUtil.php';
+
+/**
+ * Clase TelefonoDAO
+ *
+ * Esta clase implementa la interfaz CRUD y proporciona métodos para gestionar los teléfonos utilizando una base de datos.
+ */
+class TelefonoDAO implements CRUD
+{
+
+    private $link;
+    private $util;
+
+    /**
+     * Constructor de la clase.
+     * Crea una instancia de la clase Conexion y obtiene una conexión PDO.
+     * Inicializa la instancia de DAOUtil.
+     */
+    public function __construct()
+    {
+        $conexion = new Conexion();
+        $this->link = $conexion->getConexion();
+        $this->util = new DAOUtil();
+    }
+
+    /**
+     * Método para agregar un nuevo teléfono.
+     *
+     * @param Telefono $nuevoTelefono El objeto Telefono a agregar.
+     * @return void
+     */
+    public function agregar($nuevoTelefono)
+    {
+        $mapa = array(
+            ":idProveedor" => $nuevoTelefono->getIdProveedor(),
+            ":telefono" => $nuevoTelefono->getTelefono()
+        );
+        $this->util->agregar($this->link, "INSERT INTO Telefono VALUES(null, :idProveedor, :telefono)", $mapa);
+    }
+
+    /**
+     * Método para eliminar un teléfono por su ID.
+     *
+     * @param mixed $idTelefono El ID del teléfono a eliminar.
+     * @return void
+     */
+    public function eliminar($idTelefono)
+    {
+        $this->util->eliminar($this->link, "DELETE FROM Telefono WHERE id_telefono = :id", $idTelefono);
+    }
+
+    /**
+     * Método para actualizar un teléfono.
+     *
+     * @param mixed $idTelefono El ID del teléfono a actualizar.
+     * @param Telefono $nuevoTelefono El objeto Telefono con los nuevos valores.
+     * @return void
+     */
+    public function actualizar($idTelefono, $nuevoTelefono)
+    {
+        $mapa = array(
+            ":id" => $idTelefono,
+            ":idProveedor" => $nuevoTelefono->getIdProveedor(),
+            ":telefono" => $nuevoTelefono->getTelefono()
+        );
+        $this->util->agregar($this->link, "UPDATE Telefono SET id_proveedor = :idProveedor, telefono = :telefono WHERE id_telefono = :id", $mapa);
+    }
+
+    /**
+     * Método para obtener un teléfono por su ID.
+     *
+     * @param mixed $idTelefono El ID del teléfono a obtener.
+     * @return Telefono|null El objeto Telefono correspondiente al ID proporcionado, o null si no se encontró ningún teléfono.
+     */
+    public function obtenerPorId($idTelefono)
+    {
+        $query = "SELECT * FROM Telefono WHERE id_telefono = :id";
+        $telefonos = $this->util->mostrarLista($this->link, $query, array(":id" => $idTelefono));
+
+        if (count($telefonos) > 0) {
+            $telefono = $telefonos[0];
+            return new Telefono($telefono['id_telefono'], $telefono['id_proveedor'], $telefono['telefono']);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Método para obtener la lista de todos los teléfonos.
+     *
+     * @return array Un array de objetos Telefono que representa la lista de todos los teléfonos.
+     */
+    public function mostrarLista()
+    {
+        $telefonos = array();
+        $lista = $this->util->mostrarLista($this->link, "SELECT * FROM Telefono", array());
+        foreach ($lista as $telefono) {
+            $telefonos[] = new Telefono($telefono['id_telefono'], $telefono['id_proveedor'], $telefono['telefono']);
+        }
+        return $telefonos;
+    }
+}
+?>
